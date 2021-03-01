b08c78e19925a7136dcaae6ba2235f624c91fe4d
Carlos Andres Cordoba Ramos <carloscordoba@innovarweb.com>
2021-03-01 00:56:18 -0500

    Feat: Banner responsive y con funcionalidad de likes

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import * as data from '../info.json';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.less']
+  styleUrls: ['./banner.component.less'],
 })
 export class BannerComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  items: {
+    name: string;
+    description: string;
+    image: string;
+    likes: number;
+    dislikes: number;
+  }[];
+  principal: any;
+  keys: any[];
+  constructor() {
+    this.items = data;
+    this.keys = Object.entries(this.items);
+    this.principal = this.keys[0][1][0];
   }
 
+  liked():void {
+    console.log(this.principal.likes);
+    this.principal.likes = this.principal.likes +1;
+  }
+  disliked():void {
+    console.log(this.principal.dislikes);
+    this.principal.dislikes = this.principal.dislikes +1;
+  }
+  ngOnInit(): void {}
 }
